b18866a7dbb3a22febac46c220851653cf4b185a
skyhu2 <125761568+skyhu2@users.noreply.github.com>
2024-11-02 16:09:18 -0700

    Update username_password.py

--- username_password.py
+++ username_password.py
@@ -1,35 +1,43 @@
 import keyring as kr
 from flask import request, jsonify
 import json
+import os
 
-def create_account(username, password, password2):
-    #username = input("Enter Username: ")
-    #password = input("Enter Password: ")
-    #password2 = input("Re-enter your password: ")
+def create_account(username, password, password2, email):
 
     if password != password2:
-        #print("Error")
-        return "Error"
-    
+        return False
+    fin = open("UserInfo.txt", "a+")
+    fin.seek(0, os.SEEK_END)
+    if fin.tell != 0:
+        fin.seek(os.SEEK_END, 0)
+        for line in fin:
+            #print(line)
+            user = line.split(" ", 1)
+            print(user)
+            if user[0] == username:
+                return "username"
     kr.set_password("ProjectForClass", username, password)
-    return "Account Created"
+    fin.write("%s %s\n" %(username, email))
+    fin.close()
+    return True
 
 def delete_account(username, password):
-    #username = input("Enter Username: ")
-    #password = input("Enter Password: ")
-
     actual_password = kr.get_password("ProjectForClass", username)
 
     if password == actual_password:
         kr.delete_password("ProjectForClass", username)
+        fout = open("UserInfo.txt", "a+")
+        for line in fout:
+            user = line.split(" ", 1)
+            if user[0] == username:
+                del(line)
+                #fout.write(line)
     else:
-        return "Password is not correct"
-    return "Deleted"
+        return False
+    return True
 
 def login(username, password):
-    #username = input("Enter Username: ")
-    #password = input("Enter Password: ")
-
     actual_password = kr.get_password("ProjectForClass", username)
 
     if password == actual_password:
