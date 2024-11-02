0d77c948bc3678256dc2d2fd9afb7cf7edaeef46
skyhu2 <125761568+skyhu2@users.noreply.github.com>
2024-11-02 16:09:00 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import username_password
 import timebased
+import emailing
 from flask import Flask
 from view import view
 
 app = Flask(__name__)
 app.register_blueprint(view, url_prefix="/")
+
 
 #@app.route('/')
 #def home():
